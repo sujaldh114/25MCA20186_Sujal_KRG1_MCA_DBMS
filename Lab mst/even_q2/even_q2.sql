dc85ee3eac3758603dd4a2447324248844105cae
sujaldh114 <sujaldh411@gmail.com>
2026-03-10 11:51:24 +0530

    Add files via upload

---
+++ Lab mst/even_q2/even_q2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employees3 (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    manager_id INT,
+    department VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees3 VALUES
+(1,'Amit',NULL,'Management',120000),
+(2,'Ravi',1,'Engineering',80000),
+(3,'Neha',1,'Engineering',82000),
+(4, 'Karan',2,'Engineering',60000),
+(5,'Simran',2,'Engineering',62000),
+(6,'Pooja',3,'Engineering',64000),
+(7,'Rahul',3,'Engineering',64000),
+(8,'Arjun',1,'HR',70000);
+
+SELECT emp_id, emp_name, salary
+FROM employees3
+WHERE salary = (
+    SELECT MAX(salary)
+    FROM employees3
+    WHERE salary < (SELECT MAX(salary) FROM employees3)
+);
